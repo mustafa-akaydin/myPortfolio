602d5fb393f1b5c407e23f35bd45707ba533aae6
mustafa-akaydin <akaydinmustafa5@gmail.com>
2024-03-20 04:19:43 +0300

    myPortFolio

---
+++ src/components/Footer.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai'
+import { FaLinkedinIn } from 'react-icons/fa'
+
+function Footer() {
+  let date = new Date()
+  let year = date.getFullYear()
+  return (
+    <Container fluid className='footer'>
+      <Row>
+        <Col md='4' className='footer-copywright'>
+          <h3>Designed and Developed by Mustafa Akaydın</h3>
+        </Col>
+        <Col md='4' className='footer-copywright'>
+          <h3>Copyright © {year} MA</h3>
+        </Col>
+        <Col md='4' className='footer-body'>
+          <ul className='footer-icons'>
+            <li className='social-icons'>
+              <a
+                href='https://github.com/mustafa-akaydin'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className='social-icons'>
+              <a
+                href=''
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className='social-icons'>
+              <a
+                href='https://www.linkedin.com/in/mustafa-akaydn'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className='social-icons'>
+              <a
+                href='https://www.instagram.com/mustafa.akaydn/'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ src/components/Home/Type.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: ['Software Engineer',
+         'Frontend Developer'],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  )
+}
+
+export default Type;
